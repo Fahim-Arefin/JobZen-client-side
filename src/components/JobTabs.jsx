e3c3bfacbcd03c26319e60c3c46b996d2477bd75
Fahim <fahimarefin57@gmail.com>
2023-11-05 19:47:07 +0600

    Jobs Tabs component is created

---
+++ src/components/JobTabs.jsx
@@ -0,0 +1,25 @@
+const JobTabs = ({ tabData, activeTab, setActiveTab, className }) => {
+  const handleTabClick = (tabId) => {
+    setActiveTab(tabId);
+  };
+
+  return (
+    <div className={`w-full mx-auto max-w-screen-lg ${className}`}>
+      <div className="flex flex-wrap">
+        {tabData.map((tab) => (
+          <div
+            key={tab.id}
+            onClick={() => handleTabClick(tab.id)}
+            className={`w-full md:flex-1 p-4 text-center cursor-pointer ${
+              activeTab === tab.id ? "border-b-2 border-[#9685e2]" : "border-b"
+            }`}
+          >
+            {tab.label}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default JobTabs;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,13 +1,30 @@
 import { Helmet } from "react-helmet";
 import Banner from "../components/Banner";
+import JobTabs from "../components/JobTabs";
+import { useState } from "react";
 
 function Home() {
+  const [activeTab, setActiveTab] = useState("all-jobs");
+
+  const tabData = [
+    { id: "all-jobs", label: "All Jobs" },
+    { id: "on-site", label: "On Site Job" },
+    { id: "remote", label: "Remote Job" },
+    { id: "hybrid", label: "Hybrid" },
+    { id: "part-time", label: "Part Time" },
+  ];
   return (
     <div className=" grow">
       <Helmet>
         <title>JobZen | Home</title>
       </Helmet>
       <Banner />
+      <JobTabs
+        tabData={tabData}
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+        className="mb-24"
+      />
     </div>
   );
 }
